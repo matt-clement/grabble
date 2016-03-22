a44d060c9500d64f8d1526bf401c8209fb2533af
Matt Clement <clement.matthewp@gmail.com>
2016-03-21 21:15:23 -0400

    Initial commit, just a single file atm

---
+++ test.rb
@@ -0,0 +1,196 @@
+module Grabble
+  class Vertex
+    def initialize(data)
+      @data = data
+    end
+
+    def data
+      @data
+    end
+
+  end
+
+  class Edge
+    def initialize(v1, v2)
+      @vertex1 = v1
+      @vertex2 = v2
+    end
+
+    def other(vertex)
+      vtx = vertices.find(vertex)
+      return nil unless vtx
+      vertices.reject{|v| v == vertex}.first
+    end
+
+    def vertices
+      [@vertex1, @vertex2]
+    end
+  end
+
+  class Cache
+
+    def options
+      @options ||= {
+        data_filters: [lambda {|x| x.match(/^[a-zA-Z]+$/)}],
+        partitions: [lambda {|x| x.length}]
+      }
+    end
+
+    def vertices
+      @vertices ||= Hash.new {|hash, key| p "Creating new partition: #{key} | #{key.class}"; hash[key] = Array.new}
+    end
+
+    def total_vertices
+      count = 0
+      vertices.each_value{|va| count += va.size}
+      count
+    end
+
+    def edges
+      @edges ||= []
+    end
+
+    def clear
+      @vertices = nil
+      @edges = nil
+      @options = nil
+    end
+
+    def clear_options
+      @options = nil
+    end
+
+    def partition_key(data)
+      partition_key = options[:partitions].map{ |f|
+        f.call(data)
+      }.join('')
+    end
+
+    def partition(vertex)
+      data = if vertex.is_a? Grabble::Vertex
+               vertex.data
+             else
+               vertex
+             end
+      vertices[partition_key(data)]
+    end
+
+    def random(partition_key, max_depth)
+      selected_vertices = []
+      return nil if max_depth < 1
+      vertex = vertices[partition_key].sample
+      return nil unless vertex
+      
+      adjacent_vertices(vertex).each{ |v|
+        selected_vertices << v
+        selected_vertices.concat(Array(random(partition_key(v), max_depth - 1)))
+      }
+      selected_vertices.uniq
+    end
+
+    def relevant_edges(vertex)
+      vertex = find_vertex(vertex) unless vertex.is_a? Grabble::Vertex
+      edges.select{|e| e.vertices.include? vertex}
+    end
+
+    def adjacent_vertices(vertex)
+      vertex = find_vertex(vertex) unless vertex.is_a? Grabble::Vertex
+      relevant_edges(vertex).map{|e| e.other(vertex)}
+    end
+
+    def add_vertex(obj)
+      find_or_create_vertex(obj)
+    end
+
+    def delete_vertex(obj)
+      ev = find_vertex(obj)
+      return nil unless ev
+      edges.reject!{|e| e.vertices.include?(ev)}
+      partition(ev).delete(ev)
+    end
+
+    def find_vertex(obj)
+      if obj.is_a? Grabble::Vertex
+        partition(obj).include? obj
+        obj
+      else
+        vertices[partition_key(obj)].find{|x| x.data == obj}
+      end
+    end
+
+    def sort_vertices(part)
+      vertices[part].sort!{|v1, v2| v1.data <=> v2.data}
+    end
+
+    def filter_vertex_data(obj)
+      data = obj.downcase
+      filter_values = if options[:data_filters]
+        options[:data_filters].map do |f|
+          f.call(data)
+        end 
+      else
+        [true]
+      end
+
+      filter_values.reduce(true){|acc, val| acc && val} ? data : nil
+    end
+
+
+    def find_or_create_vertex(obj)
+      ev = find_vertex(obj)
+      if ev
+        vertex = ev
+      else
+        data = filter_vertex_data(obj)
+        return nil unless data
+        part = partition_key(data)
+        vertex = Vertex.new(data)
+        vertices[part] << vertex
+        create_edges(vertex)
+        sort_vertices(part)
+      end
+      vertex
+    end
+
+    def add_edge(v1, v2)
+      find_or_create_edge(v1, v2)
+    end
+
+    def likeness(str1, str2)
+      chars = str1.chars
+      counter = 0
+      str2.chars.each.with_index do |ch, index|
+        counter+=1 if chars[index] == ch
+      end
+      counter
+    end
+
+    def create_edges(vertex)
+      str1 = vertex.data
+      similar = partition(vertex).select do |v|
+        str2 = v.data
+        str1.length == str2.length &&
+          likeness(str1, str2) == vertex.data.length - 1
+      end
+      similar.each do |v|
+        add_edge(v, vertex)
+      end
+    end
+
+    def find_or_create_edge(a, b)
+      v1 = find_or_create_vertex(a)
+      v2 = find_or_create_vertex(b)
+      ee = edges.
+        select{|e| e.vertices.include? v1}.
+        find{|e| e.vertices.include? v2}
+      if ee
+        edge = ee
+      else
+        edge = Edge.new(v1, v2)
+        puts "Created edge: #{v1.data} | #{v2.data}"
+        edges << edge
+      end
+      edge
+    end
+  end
+end
