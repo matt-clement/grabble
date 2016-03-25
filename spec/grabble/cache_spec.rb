a59ccbe5cea7336962261e3eb31ac509657c951c
Matt Clement <clement.matthewp@gmail.com>
2016-03-25 14:34:35 -0400

    Ignore rspec's persistant test file

--- spec/grabble/cache_spec.rb
+++ spec/grabble/cache_spec.rb
@@ -2,9 +2,16 @@ require 'grabble.rb'
 RSpec.describe Grabble::Cache do
 
   describe '#add_vertex' do
-    it 'automatically creates a new partition if needed' do
+    it 'creates a new partition if needed' do
       test_cache = Grabble::Cache.new
       expect{test_cache.add_vertex("test")}.to change{test_cache.vertices.keys}.from([]).to(["4"])
     end
+
+    it 'creates new edges if needed' do
+      test_cache = Grabble::Cache.new
+      test_cache.add_vertex("test")
+      expect { test_cache.add_vertex("lest") }.to change{test_cache.edges.size}.by 1
+    end
+
   end
 end
